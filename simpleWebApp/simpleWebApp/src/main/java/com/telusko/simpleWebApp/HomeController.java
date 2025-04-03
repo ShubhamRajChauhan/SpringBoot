fbdd7e382ca1a63435df95733baad5b59df25ac5
Shubham <shubhamrajchauhan07@gmail.com>
2025-04-03 23:45:10 +0530

    Spring Boot Web

--- simpleWebApp/simpleWebApp/src/main/java/com/telusko/simpleWebApp/HomeController.java
+++ simpleWebApp/simpleWebApp/src/main/java/com/telusko/simpleWebApp/HomeController.java
@@ -1,4 +1,20 @@
 package com.telusko.simpleWebApp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @RequestMapping("/")
+    public  String greet() {
+        return "Welcome to Telusko!!!";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "Jai Shree Ram";
+    }
 }
