37580939c1e35b7e8f4a73f01029e988b1e9d005
Shubham <shubhamrajchauhan07@gmail.com>
2025-04-08 00:25:14 +0530

    PUT and DELETE

--- simpleWebApp/simpleWebApp/src/main/java/com/telusko/simpleWebApp/service/ProductService.java
+++ simpleWebApp/simpleWebApp/src/main/java/com/telusko/simpleWebApp/service/ProductService.java
@@ -20,13 +20,35 @@ public class ProductService {
         return products;
     }
 
+
     public Product getProductById(int prodId) {
         return products.stream()
                 .filter(p -> p.getProdId() == prodId)
                 .findFirst().orElse(new Product(100, "No Item", 0));
     }
 
+
     public void addProduct(Product prod) {
         products.add(prod);
+    }
+
+
+    public void updateProduct(Product prod) {
+        int index = 0;
+        for(int i=0; i<products.size(); i++)
+            if(products.get(i).getProdId() == prod.getProdId())
+                index = i;
+
+        products.set(index,prod);
+    }
+
+
+    public void deleteProduct(int prodId) {
+        int index = 0;
+        for(int i=0; i<products.size(); i++)
+            if(products.get(i).getProdId() == prodId)
+                index = i;
+
+        products.remove(index);
     }
 }
